a5ac3cc3cbda26153ae62ae8310e8f6cb45bf7a7
MariMax <maxim.prohachev@yandex.ru>
2024-06-04 17:14:23 +0300

    исправление ветки testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -18,9 +18,9 @@ public class CashbackHackServiceTest {
 
     public void testAmountIsMoreThan1000() {
         CashbackHackService service = new CashbackHackService();
-        int amount = 3000;
+        int amount = 1700;
         int actual = service.remain(amount);
-        int expected = 0;
+        int expected = 300;
         assertEquals(actual, expected);
     }
 
@@ -41,6 +41,15 @@ public class CashbackHackServiceTest {
         int amount = 0;
         int actual = service.remain(amount);
         int expected = 1000;
+        assertEquals(actual, expected);
+    }
+    @org.testng.annotations.Test
+
+    public void testAmountIsMultipleOf1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 3000;
+        int actual = service.remain(amount);
+        int expected = 0;
         assertEquals(actual, expected);
     }
 }
